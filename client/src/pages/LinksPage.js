a6aedbe27001d84cf0a25a57683d7984aecf57e7
Vyachaslau <farmik222@yandex.ru>
2020-10-31 17:10:49 +0300

    initial

---
+++ client/src/pages/LinksPage.js
@@ -0,0 +1,36 @@
+import React, { useCallback, useContext, useEffect, useState } from 'react';
+import { LinkList } from '../components/LinkList';
+import { Loader } from '../components/Loader';
+import { AuthContext } from '../context/Auth.Context';
+import { useHttp } from '../hooks/http.hook';
+
+export const LinksPage = () => {
+	const [ links, setLinks ] = useState([]);
+	const { loading, request } = useHttp();
+	const { token } = useContext(AuthContext);
+
+	const fetchLinks = useCallback(
+		async () => {
+			try {
+				const fetched = await request('/api/link', 'GET', null, {
+					Authorization: `Bearer ${token}`
+				});
+				setLinks(fetched);
+			} catch (e) {}
+		},
+		[ token, request ]
+	);
+
+	useEffect(
+		() => {
+			fetchLinks();
+		},
+		[ fetchLinks ]
+	);
+
+	if (loading) {
+		return <Loader />;
+	}
+
+	return <React.Fragment>{!loading && <LinkList links={links} />}</React.Fragment>;
+};
